648b8d3519065bd72e6916a2d9d8ef949090b749
RafaelRMJesus <rafaelrocha191919@gmail.com>
2024-02-08 12:13:57 -0300

    feat: delete student endpoint

---
+++ controllers/delete-student.go
@@ -0,0 +1,16 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/RafaelRMJesus/zlsx-api/db"
+	"github.com/RafaelRMJesus/zlsx-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func DeleteStudent(c *gin.Context) {
+	var student models.Student
+	id := c.Params.ByName("id")
+	db.DB.Delete(&student, id)
+	c.JSON(http.StatusOK, "Student sucessfully deleted")
+}
