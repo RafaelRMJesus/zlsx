84c2053eeb2aa7fae6e4995b22d98b58532c0f1c
RafaelRMJesus <rafaelrocha191919@gmail.com>
2024-02-08 21:22:04 -0300

    fix: get student by Id err parameter broke

--- controllers/get-student-byId.go
+++ controllers/get-student-byId.go
@@ -12,7 +12,7 @@ func GetStudentById(c *gin.Context) {
 	var student models.Student
 	id := c.Params.ByName("id")
 	db.DB.Find(&student, id)
-	if student.Name == "" {
+	if student.ID == 0 {
 		c.JSON(http.StatusNotFound, "Student Not Found")
 		return
 	}
--- routes/routes.go
+++ routes/routes.go
@@ -9,7 +9,10 @@ func InitRoutes() {
 	r := gin.Default()
 	r.GET("/students", controller.ShowStudents)
 	r.POST("/students", controller.CreateStudent)
-	r.GET("/students/:id", controller.GetStudentById)
+	r.GET("/students/id/:id", controller.GetStudentById)
+	r.GET("/students/name/:name", controller.SearchStudentByName)
+	r.GET("students/deleted", controller.GetDeleted)
 	r.DELETE("students/:id", controller.DeleteStudent)
+	r.PATCH("/students/:id", controller.UpdateStudent)
 	r.Run()
 }
