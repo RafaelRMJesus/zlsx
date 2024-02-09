e8656574119e47860f4eb0c394ccb5bb086c120b
RafaelRMJesus <rafaelrocha191919@gmail.com>
2024-02-08 21:21:15 -0300

    feat: update students endpoint

---
+++ controllers/update-students.go
@@ -0,0 +1,24 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/RafaelRMJesus/zlsx-api/db"
+	"github.com/RafaelRMJesus/zlsx-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func UpdateStudent(c *gin.Context) {
+	var student models.Student
+	id := c.Params.ByName("id")
+	db.DB.First(&student, id)
+	if err := c.ShouldBindJSON(&student); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	db.DB.Model(&student).UpdateColumns(student)
+	c.JSON(http.StatusOK, student)
+}
