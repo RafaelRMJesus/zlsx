27f748383fcff1479afe3dc7411e9e9712bb6774
RafaelRMJesus <rafaelrocha191919@gmail.com>
2024-02-19 14:32:50 -0300

    feat: test get all students endpoint

--- main_test.go
+++ main_test.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	controller "github.com/RafaelRMJesus/zlsx-api/controllers"
+	"github.com/RafaelRMJesus/zlsx-api/db"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
 func SetupTestRoutes() *gin.Engine {
@@ -20,7 +23,18 @@ func TestSayHello(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/rafa", nil)
 	res := httptest.NewRecorder()
 	r.ServeHTTP(res, req)
-	if res.Code != http.StatusOK {
-		t.Fatalf("Status error: reveived %d, but expected %d", res.Code, http.StatusOK)
-	}
+	assert.Equal(t, http.StatusOK, res.Code, "expected %d, but got %d", http.StatusOK, res.Code)
+	mockRes := `"Eai rafa, tudo bem?"`
+	resBody, _ := io.ReadAll(res.Body)
+	assert.Equal(t, mockRes, string(resBody))
+}
+
+func TestGetAllStudents(t *testing.T) {
+	db.DbConnect()
+	r := SetupTestRoutes()
+	r.GET("/students", controller.ShowStudents)
+	req, _ := http.NewRequest("GET", "/students", nil)
+	res := httptest.NewRecorder()
+	r.ServeHTTP(res, req)
+	assert.Equal(t, http.StatusOK, res.Code)
 }
